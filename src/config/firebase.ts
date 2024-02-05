204e99d9a0995d3c3bb3f8989e0e621e75bdfe4f
UsmanBaig001 <usmanbaig1572@gmail.com>
2024-02-05 18:04:28 +0500

    first-commit

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,16 +1,16 @@
 
-// import { initializeApp } from "firebase/app";
-// import {getAuth} from "firebase/auth"
+// import { initializeApp } from "@react-native-firebase/app"
+// import {getAuth} from "@react-native-firebase"
 // import {getFirestore} from "firebase/firestore"
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAcCLxPQ21PTNjIjZwa7CBSbnYorvPr7yE",
-  authDomain: "native-module-2.firebaseapp.com",
-  projectId: "native-module-2",
-  storageBucket: "native-module-2.appspot.com",
-  messagingSenderId: "706657334560",
-  appId: "1:706657334560:web:ef372fac7d05761c65d345"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAcCLxPQ21PTNjIjZwa7CBSbnYorvPr7yE",
+//   authDomain: "native-module-2.firebaseapp.com",
+//   projectId: "native-module-2",
+//   storageBucket: "native-module-2.appspot.com",
+//   messagingSenderId: "706657334560",
+//   appId: "1:706657334560:web:ef372fac7d05761c65d345"
+// };
 
 // export const FIREBASE_APP = initializeApp(firebaseConfig);
 // export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
